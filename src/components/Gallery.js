3ab020c9fd99899e79182cc2e1776743ee08ef32
OlSavMe <olga_savka.os@mail.ru>
2020-12-20 23:25:35 +0200

    zoom in / out on lightbox

--- src/components/Gallery.js
+++ src/components/Gallery.js
@@ -1,4 +1,7 @@
 import React, { useState } from "react";
+import Close from "../assets/koho_close white.svg";
+import NextArrow from "../assets/koho_next arrow white.svg";
+import PrevArrow from "../assets/koho_previous arrow white.svg";
 
 const Gallery = (props) => {
   const images = props.item.fields.projectGallery;
@@ -6,6 +9,30 @@ const Gallery = (props) => {
 
   const [lightbox, setLightbox] = useState(false);
   const [imageToShow, setImageToShow] = useState("");
+  const [doubleClick, setDoubleClick] = useState(false);
+
+  const [touchStart, setTouchStart] = React.useState(0);
+  const [touchEnd, setTouchEnd] = React.useState(0);
+
+  const handleTouchStart = (e) => {
+    setTouchStart(e.targetTouches[0].clientX);
+  };
+
+  const handleTouchMove = (e) => {
+    setTouchEnd(e.targetTouches[0].clientX);
+  };
+
+  const handleTouchEnd = () => {
+    if (touchStart - touchEnd > 70) {
+      // do your stuff here for left swipe
+      showNext();
+    }
+
+    if (touchStart - touchEnd < -70) {
+      // do your stuff here for right swipe
+      showPrev();
+    }
+  };
 
   const showImage = (image) => {
     //set imageToShow to be the one that's been clicked on
@@ -20,7 +47,7 @@ const Gallery = (props) => {
 
   //show next image in lightbox
   const showNext = (e) => {
-    e.stopPropagation();
+    // e.stopPropagation();
     let currentIndex = images.indexOf(imageToShow);
     if (currentIndex >= images.length - 1) {
       setLightbox(false);
@@ -32,7 +59,7 @@ const Gallery = (props) => {
 
   //show previous image in lightbox
   const showPrev = (e) => {
-    e.stopPropagation();
+    // e.stopPropagation();
     let currentIndex = images.indexOf(imageToShow);
     if (currentIndex <= 0) {
       setLightbox(false);
@@ -40,6 +67,10 @@ const Gallery = (props) => {
       let nextImage = images[currentIndex - 1];
       setImageToShow(nextImage);
     }
+  };
+
+  const toggleDoubleClick = (e) => {
+    setDoubleClick(!doubleClick);
   };
 
   return (
@@ -55,26 +86,31 @@ const Gallery = (props) => {
       ))}
       {lightbox ? (
         <div id="lightbox">
-          <span className="right" onClick={hideLightBox}>
-            &#9587;
-          </span>
+          <img
+            className="close-icon"
+            src={Close}
+            alt="close"
+            onClick={hideLightBox}
+          />
 
-          <span className="next" onClick={showNext}>
-            {" "}
-            &#8963;
-          </span>
+          <img className="next" src={NextArrow} alt="next" onClick={showNext} />
 
-          <span className="prev" onClick={showPrev}>
-            {" "}
-            &#8963;
-          </span>
+          <img className="prev" src={PrevArrow} alt="prev" onClick={showPrev} />
 
-          <div className="img-wrapper">
+          <div
+            className={doubleClick ? "img-wrapper-dbc" : "img-wrapper"}
+            onTouchStart={handleTouchStart}
+            onTouchMove={handleTouchMove}
+            onTouchEnd={handleTouchEnd}
+          >
             <img
               id="lightbox-img"
               src={imageToShow.fields.file.url}
               alt="img-this"
+              onDoubleClick={toggleDoubleClick}
             />
+            <span id="tooltip-in">Doubleclick to zoom in / out</span>
+            {/* <span id="tooltip-out">Doubleclick to zoom out</span> */}
           </div>
         </div>
       ) : (
